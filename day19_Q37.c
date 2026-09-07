01a3698a3be4160f5b56a5db8863e5d034bbf07d
Mudit Gangwar <mudit11126@gmail.com>
2026-09-08 01:16:32 +0530

    added day19 Q37 code

---
+++ day19_Q37.c
@@ -0,0 +1,21 @@
+//Write a program to find the LCM of two numbers.
+#include <stdio.h>
+
+int main() {
+    int num1, num2, i, hcf, lcm;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &num1, &num2);
+
+    for (i = 1; i <= num1; i++) {
+        if (num1 % i == 0 && num2 % i == 0) {
+            hcf = i;
+        }
+    }
+
+    lcm = (num1 * num2) / hcf;
+
+    printf("LCM is: %d\n", lcm);
+
+    return 0;
+}

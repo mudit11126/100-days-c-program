1b9e41ab442c42478c902a0a41ccd80e9eb2cd78
Mudit Gangwar <mudit11126@gmail.com>
2026-09-08 01:00:59 +0530

    added day17 Q34 code

---
+++ day17_Q34.c
@@ -0,0 +1,24 @@
+//Write a program to check if a number is prime.
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    int count = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++) {
+        if (n % i == 0) {
+            count++;
+        }
+    }
+
+    if (count == 2) {
+        printf("%d is a Prime number.\n", n);
+    } else {
+        printf("%d is not a Prime number.\n", n);
+    }
+
+    return 0;
+}
